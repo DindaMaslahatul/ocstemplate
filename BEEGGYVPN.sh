b755a4e8a67d90715bd4b8595d163c1b3b6758ff
beeggy <44807394+beeggy@users.noreply.github.com>
2019-01-02 20:02:43 +0800

    Update BEEGGYVPN.sh

--- BEEGGYVPN.sh
+++ BEEGGYVPN.sh
@@ -2,7 +2,12 @@
 echo "Removing Old Theme"
 rm -rf /home/vps/public_html/asset
 rm -rf /home/vps/public_html/view
-rm -rf /home/vps/public_html/tmp/*
+rm -rf /home/vps/public_html/config
+rm -rf /home/vps/public_html/controller
+rm -rf /home/vps/public_html/file
+rm -rf /home/vps/public_html/lib
+rm -rf /home/vps/public_html/model
+rm -rf /home/vps/public_html/tmp
 echo "Installing BEEGGYVPN Theme"
 cd ~
 mkdir Beeggy
@@ -11,4 +16,10 @@ rm -rf *
 wget https://www.dropbox.com/s/hd2b85rfwukce1r/jerz.tar.gz && tar zxvf jerz.tar.gz
 mv asset /home/vps/public_html
 mv view /home/vps/public_html
+mv config /home/vps/public_html
+mv controller /home/vps/public_html
+mv file /home/vps/public_html
+mv lib /home/vps/public_html
+mv model /home/vps/public_html
+mv tmp /home/vps/public_html
 echo "Enjoy using BEEGGYVPN Theme -Beeggy"
